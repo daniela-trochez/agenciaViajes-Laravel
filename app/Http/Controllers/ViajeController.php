c6e74641d9a874cc411bfe2e8ff7a62d44cb62b7
unknown <calidad8844@gmail.com>
2024-07-09 11:25:53 -0500

    vista viajes

--- app/Http/Controllers/ViajeController.php
+++ app/Http/Controllers/ViajeController.php
@@ -2,49 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destino;
+use App\Models\Origen;
 use App\Models\Viaje;
+use App\Models\Viajero;
 use Illuminate\Http\Request;
 
 class ViajeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        $viajes= Viaje::with('viajero','destino','origen')->get();
+        return view('viajes.index', compact('viajes'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $viajeros= Viajero::all();
+        $destinos= Destino::all();
+        $origens= Origen::all();
+        return view('viajes.create', compact('viajeros','destinos','origens'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(Request $request)
     {
-        //
+        Viaje::create($request->all());
+        return redirect()->route('viajeros.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(Viaje $viaje)
     {
-        //
+        return view('viajes.show', compact('viaje'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(Viaje $viaje)
     {
-        //
+        $viaje->update();
     }
 
     /**
---
+++ resources/views/viajes/index.blade.php
@@ -0,0 +1,7 @@
+<h1>Registro de Viajes</h1>
+<a href="crear viaje"></a> <br>
+<ul>
+    <li>
+        
+    </li>
+</ul>
